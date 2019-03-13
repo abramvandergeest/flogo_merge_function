a10f46200de86cdfdf5bb0ebedc791c571165565
Abram Van Der Geest <avanderg@tibco.com>
2019-03-13 10:39:32 -0400

    very basic merge of maps non-arrays with one index between them

--- merge.go
+++ merge.go
@@ -26,40 +26,22 @@ func (fnRandom) Sig() (paramTypes []data.Type, isVariadic bool) {
 }
 
 func (fnRandom) Eval(params ...interface{}) (interface{}, error) {
-	var outmap map[string][]interface{}
-	var indexset map[interface{}][]int
 	maps := params[0].([]map[string]interface{})
 	mergeKey := params[1].([]string)
-	keymap := map[string]int{}
+	var output map[string]interface{}
+
+	indval := maps[0][mergeKey[0]]
 
 	for i, m := range maps {
-		k := mergeKey[i]
-
-		if len(m[k].([]interface{})) > 1 {
-			for j, _ := range m[k].([]interface{}) {
-				indexset[m[k]][i] = j
-			}
-		} else {
-			indexset[m[k]][i] = 0
+		if mergeKey[i] != indval {
+			return nil, fmt.Errorf("there exists multiple indexs, currently we are assuming one index")
 		}
-
-		for key := range m {
-			if _, ok := keymap[key]; !ok && key != mergeKey[i] {
-				keymap[key] = i
+		for key, val := range m {
+			if key != mergeKey[i] || i == 0 {
+				output[key] = val
 			}
-			// else{    //IF THE KEY IS A DUPLICATE
-			// 	keymap[]
-			// }
 		}
-		blah := m[k]
-		fmt.Println(blah)
 	}
 
-	fmt.Println(mergeKey, maps, outmap, indexset)
-	// limit := 10
-	// if len(params) > 0 {
-	// 	limit = params[0].(int)
-	// }
-	// rand.Seed(time.Now().UnixNano())
-	return 0, nil
+	return output, nil
 }
