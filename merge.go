695c23fe8a23e6478d7b65f77b74d2b78d0489fc
Abram Van Der Geest <avanderg@tibco.com>
2019-03-13 11:15:57 -0400

    getting the basic function to work

--- merge.go
+++ merge.go
@@ -11,21 +11,21 @@ import (
 )
 
 func init() {
-	function.Register(&fnRandom{})
+	function.Register(&fnMerge{})
 }
 
-type fnRandom struct {
+type fnMerge struct {
 }
 
-func (fnRandom) Name() string {
+func (fnMerge) Name() string {
 	return "merge"
 }
 
-func (fnRandom) Sig() (paramTypes []data.Type, isVariadic bool) {
-	return []data.Type{data.TypeInt}, true
+func (fnMerge) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeArray, data.TypeArray}, false
 }
 
-func (fnRandom) Eval(params ...interface{}) (interface{}, error) {
+func (fnMerge) Eval(params ...interface{}) (interface{}, error) {
 	maps := params[0].([]map[string]interface{})
 	mergeKey := params[1].([]string)
 	var output map[string]interface{}
@@ -33,7 +33,7 @@ func (fnRandom) Eval(params ...interface{}) (interface{}, error) {
 	indval := maps[0][mergeKey[0]]
 
 	for i, m := range maps {
-		if mergeKey[i] != indval {
+		if m[mergeKey[i]] != indval {
 			return nil, fmt.Errorf("there exists multiple indexs, currently we are assuming one index")
 		}
 		for key, val := range m {
