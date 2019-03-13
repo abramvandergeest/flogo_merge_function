0a57ebe44dc39240b50b12ba08d9cb59fb3e219d
Abram Van Der Geest <avanderg@tibco.com>
2019-03-13 11:38:53 -0400

    fixing input some more

--- merge.go
+++ merge.go
@@ -26,17 +26,17 @@ func (fnMerge) Sig() (paramTypes []data.Type, isVariadic bool) {
 }
 
 func (fnMerge) Eval(params ...interface{}) (interface{}, error) {
-	maps := params[0].([]map[string]interface{})
+	maps := params[0].([]interface{})
 	mergeKey := params[1].(string)
 	var output map[string]interface{}
 
-	indval := maps[0][mergeKey]
+	indval := maps[0].(map[string]interface{})[mergeKey]
 
 	for i, m := range maps {
-		if m[mergeKey] != indval {
+		if m.(map[string]interface{})[mergeKey] != indval {
 			return nil, fmt.Errorf("there exists multiple indexs, currently we are assuming one index")
 		}
-		for key, val := range m {
+		for key, val := range m.(map[string]interface{}) {
 			if key != mergeKey || i == 0 {
 				output[key] = val
 			}
